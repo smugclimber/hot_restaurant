ae112420a37b9026d692bba16080cce4cc050736
unknown <smugclimber@icloud.com>
2017-11-16 14:07:37 -0500

    broken server code

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ var app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Hot Rest tables (DATA)
 // =============================================================
 var tables = [
